0833cd12d389c4be4f7d91120b27ed42882314fb
akshay.rein-at-908805445373 <waizaeer4@gmail.com>
2022-06-04 15:08:40 +0530

    fifth

--- src/screens/cart.jsx
+++ src/screens/cart.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import "./styles.css";
+import { useSelector } from "react-redux";
+import { ItemCard } from "../component";
 
 export default function Cart(props) {
-  return <div className="cart-container"></div>;
+  const { cartItems } = useSelector((state) => state.cart);
+  return (
+    <div className="cart-container">
+      {cartItems.map((item) => (
+        <ItemCard data={item} key={item.id} />
+      ))}
+    </div>
+  );
 }
